390d9f4a0e1ed81836295f18cb3c39a6a196799e
saturn68 <jungmin.park0728@gmail.com>
2026-06-06 12:27:25 +0900

    Update map.c

--- map.c
+++ map.c
@@ -1,4 +1,16 @@
+<<<<<<< Updated upstream
 ﻿#include "game.h"
+=======
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+#define TITLE 4
+
+extern int *difficulty;
+short* main_window();
+void window_draw(short flag_window[window_row][window_col]);
+int count();
+void gototitle();
+>>>>>>> Stashed changes
 
 char  flag_window[window_row][window_col];   /* 테두리용으로만 유지 */
 short map_data[MAP_SIZE][MAP_SIZE] = { 0 };
@@ -109,9 +121,38 @@ void map_main(int* diff)
 
     srand((unsigned)time(NULL));
 
+<<<<<<< Updated upstream
     for (i = 0; i < MAP_SIZE; i++)
         for (j = 0; j < MAP_SIZE; j++)
             map_data[j][i] = ' ';
+=======
+	map_data[MAP_SIZE - 2][1] = 'b'; 
+	goal_flag();  //골인깃발
+	for (i = 0; i < MAP_SIZE; i++)
+	{
+		map_data[0][i] = '#';            
+		map_data[i][0] = '#';            
+		map_data[MAP_SIZE - 1][i] = '#'; 
+		map_data[i][MAP_SIZE - 1] = '#'; 
+	}
+		
+	switch (*diff) {
+	case EASY:
+		penalty_flag(*diff); //패널티깃발
+		good_flag(*diff);  // 보상깃발
+		break;
+	case NORMAL:
+		penalty_flag(*diff);
+		good_flag(*diff);
+		break;
+	case HARD:
+		penalty_flag(*diff);
+		good_flag(*diff);
+		break;
+	case TITLE:
+		gototitle();
+	}
+>>>>>>> Stashed changes
 
     px = 1;
     py = MAP_SIZE - 2;
@@ -136,8 +177,77 @@ void map_main(int* diff)
     counter(diff);
 }
 
+<<<<<<< Updated upstream
 /* ── 정보창: flag_window는 테두리(*) 전용으로만 사용 ─ */
 void main_window()
+=======
+void setColor(unsigned short color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}//색 입히기
+
+void map_draw() {
+	system("cls");
+	COORD baseCur = { 0, 0 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), baseCur);
+
+	int i, j;
+	for (i = 0; i < MAP_SIZE; i++) {
+		for (j = 0; j < MAP_SIZE; j++) {
+			if (map_data[i][j] == 'a')
+			{
+				setColor(4);
+				printf("▶");
+			}
+			else if (map_data[i][j] == 'c')
+			{
+				setColor(2);
+				printf("▶");
+			}
+			else if (map_data[i][j] == 'g')
+			{
+				setColor(1);
+				printf("▶");
+			}
+			else if (map_data[i][j] == 'b')
+			{
+				printf("\u25CF");
+			}
+			else if (map_data[i][j] == '#')
+			{
+				printf("■");
+			}
+			else
+				printf(" ");
+			setColor(7);
+		}
+		printf("\n");
+
+	}
+}
+
+void penalty_flag(int i) {
+	int x1, y1, j = 0;
+	for (;j < i;j++) {
+		y1 = rand() % (MAP_SIZE - 2) + 1;
+		x1 = rand() % (MAP_SIZE - 2) + 1;
+		if (!(map_data[y1][x1] == 'a' || map_data[y1][x1] == 'b' || map_data[y1][x1] == 'c' || map_data[y1][x1] == 'g'))
+			map_data[y1][x1] = 'a';
+	}
+}
+
+void good_flag(int i) {
+	int x1, y1, j = 0;
+	for (;j < i;j++) 
+	{
+		y1 = rand() % (MAP_SIZE - 2) + 1;
+		x1 = rand() % (MAP_SIZE - 2) + 1;
+		if (!(map_data[y1][x1] == 'a' || map_data[y1][x1] == 'b' || map_data[y1][x1] == 'c' || map_data[y1][x1] == 'g'))
+			map_data[y1][x1] = 'c';
+	}
+}
+
+void goal_flag() 
+>>>>>>> Stashed changes
 {
     int i, j;
     for (j = 0; j < window_row; j++)
@@ -185,8 +295,37 @@ void window_draw()
                 printf(" ");
         }
 
+<<<<<<< Updated upstream
         /* 오른쪽 테두리 */
         gotoxy(window_col - 1, MAP_SIZE + j);
         printf("%c", flag_window[j][window_col - 1]);
     }
 }
+=======
+	for (i = 0; i < window_col; i++)
+	{
+		flag_window[0][i] = '*';
+		flag_window[window_row-1][i] = '*';
+	}
+
+	for (i = 0;i < window_row;i++) {
+		flag_window[i][0] = '*';
+		flag_window[i][window_col-1] = '*';
+	}
+
+	return flag_window;
+
+	}
+
+void window_draw(short flag_window[window_row][window_col]) // 창 구현
+{
+	int i, j;
+	gotoxy(0, MAP_SIZE);
+	for (j = 0;j < window_row;j++) {
+		for (i = 0;i < window_col;i++)
+			printf("%c", flag_window[j][i]);
+		printf("\n");
+	}
+}
+
+>>>>>>> Stashed changes
